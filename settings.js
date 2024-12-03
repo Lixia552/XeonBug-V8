2f7d4af8dda4afd708a6ef1ea231ff778b0c90a1
Lixia552 <dosunmubisola@gmail.com>
2024-12-04 00:55:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,8 +10,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "2349032040887"
+global.ownername = "Rikka†"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
