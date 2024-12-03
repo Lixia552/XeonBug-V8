c20fd5891bc1f4f393c618e289a3675d0e080e49
Lixia552 <dosunmubisola@gmail.com>
2024-12-04 00:47:52 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,8 +16,8 @@ global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '2349032040887'  //creator number
+global.ownername = 'Rikka†' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
